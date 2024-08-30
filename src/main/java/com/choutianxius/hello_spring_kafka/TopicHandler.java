c36e767d8ad1adcb45a17cb86009a6bc0a246951
choutianxius <choutianxius@gmail.com>
2024-08-30 15:35:34 -0500

    Add a simple Kafka topic handler to produce messages from POST requests and consume them in logging

---
+++ src/main/java/com/choutianxius/hello_spring_kafka/TopicHandler.java
@@ -0,0 +1,38 @@
+package com.choutianxius.hello_spring_kafka;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicHandler {
+    private static final String TOPIC = "mytopic";
+    private final KafkaTemplate<String, String> template;
+
+    private final Logger logger = LoggerFactory.getLogger(TopicHandler.class);
+
+    @Autowired
+    public TopicHandler(KafkaTemplate<String, String> template) {
+        this.template = template;
+    }
+
+    @PostMapping("/")
+    public void produce(@RequestBody String message) {
+        template.send(TOPIC, message);
+    }
+
+    @KafkaListener(topics = TOPIC, groupId = "default")
+    public void consume(String in) {
+        logger.info(processMessage(in));
+    }
+
+    private static String processMessage(String rawMessage) {
+        return rawMessage.toUpperCase();
+    }
+
+}
